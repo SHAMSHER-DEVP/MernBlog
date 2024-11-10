3fe09a8461298a24b3c95db3aa9d636b9bacdc0f
shamsher ali <shamsherdevp@gmail.com>
2024-11-09 20:47:18 -0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-
+import {BrowserRouter as Br,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 function App() {
   return (
-    <>
-      <div className="sm:container  border border-red-600">
-      <button className=' m-10 p-2 bgc bt '> summit</button> <br /><br />
-      
-      <button className=' m-10 p-2 bgc bt '> summit 2</button> <br /><br />
+    <Br>
 
-      <button className=' m-10 p-2 bgc bt '> summit 3</button>
-      </div>
-
-       <h2 className='mx-13 xsm:bg-orange-700'>
-        this is for cinfigration 
-       </h2>
-
-        <div className="con">
-          <p className="c1">pagra 21</p>
-          <p className="c2">pagra 22</p>
-        </div>
-    </>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    
+    </Br>
   )
 }
 
